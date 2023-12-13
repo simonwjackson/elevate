9c9ec8a0846b121a41bfaf4d1fe1881585e06d74
Simon W. Jackson <hello@simonwjackson.io>
2023-12-12 18:13:45 -0600

    :recycle: refactor(app): extract functions to utils.ts
    - Moved gaming related functions and variables to a new utils.ts file
    - Simplified App.tsx file by removing these functions and variables
    - Refactored the processMessage function to handle JSON-RPC requests and responses
    - Added a new echo method to the JSON-RPC server

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -3,14 +3,14 @@ import * as FileSystem from "expo-file-system";
 import { WebView } from "react-native-webview";
 import { StyleSheet, Text, View } from "react-native";
 import { useAssets } from "expo-asset";
-import { StorageAccessFramework } from "expo-file-system";
-import { IntentLauncherParams, startActivityAsync } from "expo-intent-launcher";
 import { WebViewSource } from "react-native-webview/lib/WebViewTypes";
 import { StatusBar } from "expo-status-bar";
 import {
   JSONRPCRequest,
   JSONRPCServer,
   TypedJSONRPCServer,
+  isJSONRPCRequest,
+  isJSONRPCResponse,
 } from "json-rpc-2.0";
 
 // INFO: How to "write" to the download directory
@@ -36,166 +36,36 @@ type Methods = {
 };
 
 const rpcServer: TypedJSONRPCServer<Methods> = new JSONRPCServer();
+
 rpcServer.addMethod("echo", ({ message }) => {
-  alert(message);
-  return message;
+  return message + "RN";
 });
-
-const launch = (arr) => {
-  startActivityAsync(arr[0] as string, arr[1] as IntentLauncherParams)
-    .then((result) => {
-      console.log("Intent launched!", result);
-    })
-    .catch((error) => {
-      console.error("Error launching intent:", error);
-    });
-};
-
-const readGamingDir = () =>
-  StorageAccessFramework.requestDirectoryPermissionsAsync()
-    .then(async (permissions) => {
-      if (permissions.granted) {
-        const uri = permissions.directoryUri;
-
-        const files = await StorageAccessFramework.readDirectoryAsync(uri);
-        alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
-      }
-    })
-    .catch(console.log);
-
-const simpleCoreMap = {
-  "nintendo-entertainment-system":
-    "/data/data/com.retroarch.aarch64/cores/nestopia_libretro_android.so",
-  "nintendo-gameboy-advance":
-    "/data/data/com.retroarch.aarch64/cores/mgba_libretro_android.so",
-};
-
-const launcherDb = {
-  "nintendo-entertainment-system": {
-    type: "retroarch",
-    method: "intent",
-    command: [
-      "android.intent.action.MAIN",
-      {
-        category: "android.intent.category.LAUNCHER",
-        packageName: "com.retroarch.aarch64",
-        className: "com.retroarch.browser.retroactivity.RetroActivityFuture",
-        extra: {
-          // ROM: "/storage/emulated/0/tetris.nes",
-          // LIBRETRO: "/data/data/com.retroarch.aarch64/cores/nestopia_libretro_android.so",
-          CONFIGFILE:
-            "/storage/emulated/0/Android/data/com.retroarch.aarch64/files/retroarch.cfg",
-        },
-      },
-    ],
-  },
-};
-
-const buildRetroArchCommand = (game: any, theLauncher: any) => {
-  theLauncher.command[1].extra.ROM = game.file;
-  theLauncher.command[1].extra.LIBRETRO = simpleCoreMap[game.platform];
-
-  return theLauncher.command;
-};
-
-const gameDb = {
-  "😊🎉🍎🚀🌈": {
-    platform: "nintendo-entertainment-system",
-    title: "Tetris",
-    file: "/storage/emulated/0/tetris.nes",
-  },
-  ahep7fshsfh: {
-    platform: "nintendo-gameboy-advance",
-    title: "Wario Land 4",
-    file: "/storage/emulated/0/snowscape/gaming/games/nintendo-gameboy-advance/Wario Land 4 (UE) [!].gba",
-  },
-};
-
-const launchGame = (obj: any) => {
-  const games = {
-    moonlight: [
-      "android.intent.action.MAIN",
-      {
-        category: "android.intent.category.LAUNCHER",
-        packageName: "com.limelight",
-        className: "com.limelight.ShortcutTrampoline",
-        extra: {
-          UUID: "F6D480FD-C024-2820-7F02-980A469B3BF3",
-          Name: "Desktop",
-          AppId: "1577243657",
-        },
-      },
-    ],
-
-    dungreed: [
-      "android.intent.action.MAIN",
-      {
-        packageName: "com.teamhoray.dungreed",
-        className: "com.unity3d.player.UnityPlayerActivity",
-      },
-    ],
-    scourgeBringer: [
-      "android.intent.action.MAIN",
-      {
-        packageName: "com.pid.scourgebringer",
-        className: "crc645d6a1e7bece73b70.Program",
-      },
-    ],
-
-    deadCells: [
-      "android.intent.action.MAIN",
-      {
-        packageName: "com.playdigious.deadcells.mobile",
-        className: "com.playdigious.deadcells.mobile.DeadCellsLoading",
-      },
-    ],
-    locoRoco: [
-      "android.intent.action.VIEW",
-      {
-        category: "android.intent.category.DEFAULT",
-        type: "application/octet-stream",
-        packageName: "org.ppsspp.ppssppgold",
-        className: "org.ppsspp.ppsspp.PpssppActivity",
-        data: "/storage/emulated/0/psp/Loco_Roco.iso",
-      },
-    ],
-  };
-
-  // if (name === "moonlight") {
-  //   fetch("http://zao:3000/").catch(console.log);
-  // }
-
-  const theLauncher = launcherDb[gameDb[obj.id].platform];
-
-  if (theLauncher.type === "retroarch") {
-    launch(buildRetroArchCommand(gameDb[obj.id], theLauncher));
-  }
-};
-
-const processMessage = (msg: JSONRPCRequest) => {
-  rpcServer.receive(msg).then((jsonRPCResponse) => {
-    if (jsonRPCResponse) {
-      console.log(JSON.stringify(jsonRPCResponse));
-    } else {
-      // If response is absent, it was a JSON-RPC notification method.
-      // Respond with no content status (204).
-      // s.sendStatus(204);
-      alert("error");
-    }
-  });
-  // const obj = JSON.parse(msg);
-  //
-  // switch (obj.type) {
-  //   case "launch": {
-  //     launchGame(obj.payload);
-  //   }
-  // }
-};
 
 const CustomWebView = () => {
   const [source, setSource] = React.useState<WebViewSource | null>(null);
   const [assets, error] = useAssets([require("./assets/index.html")]);
-  const webViewRef = React.useRef(null);
+  const webViewRef = React.useRef<WebView>(null);
+
+  const processMessage = React.useCallback(
+    (obj: JSONRPCRequest) => {
+      // The message is a request from the frontend, we need to process it
+      if (isJSONRPCRequest(obj)) {
+        rpcServer.receive(obj).then((response) => {
+          const txt = JSON.stringify(response);
+
+          webViewRef.current?.injectJavaScript(
+            `window.receiveMessageFromReactNative(${txt})`,
+          );
+        });
+      }
+
+      // This is a direct response to a query made from here (android)
+      else if (isJSONRPCResponse(obj)) {
+        // TODO:
+      }
+    },
+    [webViewRef],
+  );
 
   React.useEffect(() => {
     if (assets) {
---
+++ apps/mobile/utils.ts
@@ -0,0 +1,133 @@
+import { StorageAccessFramework } from "expo-file-system";
+import { IntentLauncherParams, startActivityAsync } from "expo-intent-launcher";
+
+const launch = (arr) => {
+  startActivityAsync(arr[0] as string, arr[1] as IntentLauncherParams)
+    .then((result) => {
+      console.log("Intent launched!", result);
+    })
+    .catch((error) => {
+      console.error("Error launching intent:", error);
+    });
+};
+
+const readGamingDir = () =>
+  StorageAccessFramework.requestDirectoryPermissionsAsync()
+    .then(async (permissions) => {
+      if (permissions.granted) {
+        const uri = permissions.directoryUri;
+
+        const files = await StorageAccessFramework.readDirectoryAsync(uri);
+        alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
+      }
+    })
+    .catch(console.log);
+
+const simpleCoreMap = {
+  "nintendo-entertainment-system":
+    "/data/data/com.retroarch.aarch64/cores/nestopia_libretro_android.so",
+  "nintendo-gameboy-advance":
+    "/data/data/com.retroarch.aarch64/cores/mgba_libretro_android.so",
+};
+
+const launcherDb = {
+  "nintendo-entertainment-system": {
+    type: "retroarch",
+    method: "intent",
+    command: [
+      "android.intent.action.MAIN",
+      {
+        category: "android.intent.category.LAUNCHER",
+        packageName: "com.retroarch.aarch64",
+        className: "com.retroarch.browser.retroactivity.RetroActivityFuture",
+        extra: {
+          // ROM: "/storage/emulated/0/tetris.nes",
+          // LIBRETRO: "/data/data/com.retroarch.aarch64/cores/nestopia_libretro_android.so",
+          CONFIGFILE:
+            "/storage/emulated/0/Android/data/com.retroarch.aarch64/files/retroarch.cfg",
+        },
+      },
+    ],
+  },
+};
+
+const buildRetroArchCommand = (game: any, theLauncher: any) => {
+  theLauncher.command[1].extra.ROM = game.file;
+  theLauncher.command[1].extra.LIBRETRO = simpleCoreMap[game.platform];
+
+  return theLauncher.command;
+};
+
+const gameDb = {
+  "😊🎉🍎🚀🌈": {
+    platform: "nintendo-entertainment-system",
+    title: "Tetris",
+    file: "/storage/emulated/0/tetris.nes",
+  },
+  ahep7fshsfh: {
+    platform: "nintendo-gameboy-advance",
+    title: "Wario Land 4",
+    file: "/storage/emulated/0/snowscape/gaming/games/nintendo-gameboy-advance/Wario Land 4 (UE) [!].gba",
+  },
+};
+
+const launchGame = (obj: any) => {
+  const games = {
+    moonlight: [
+      "android.intent.action.MAIN",
+      {
+        category: "android.intent.category.LAUNCHER",
+        packageName: "com.limelight",
+        className: "com.limelight.ShortcutTrampoline",
+        extra: {
+          UUID: "F6D480FD-C024-2820-7F02-980A469B3BF3",
+          Name: "Desktop",
+          AppId: "1577243657",
+        },
+      },
+    ],
+
+    dungreed: [
+      "android.intent.action.MAIN",
+      {
+        packageName: "com.teamhoray.dungreed",
+        className: "com.unity3d.player.UnityPlayerActivity",
+      },
+    ],
+    scourgeBringer: [
+      "android.intent.action.MAIN",
+      {
+        packageName: "com.pid.scourgebringer",
+        className: "crc645d6a1e7bece73b70.Program",
+      },
+    ],
+
+    deadCells: [
+      "android.intent.action.MAIN",
+      {
+        packageName: "com.playdigious.deadcells.mobile",
+        className: "com.playdigious.deadcells.mobile.DeadCellsLoading",
+      },
+    ],
+    locoRoco: [
+      "android.intent.action.VIEW",
+      {
+        category: "android.intent.category.DEFAULT",
+        type: "application/octet-stream",
+        packageName: "org.ppsspp.ppssppgold",
+        className: "org.ppsspp.ppsspp.PpssppActivity",
+        data: "/storage/emulated/0/psp/Loco_Roco.iso",
+      },
+    ],
+  };
+
+  // if (name === "moonlight") {
+  //   fetch("http://zao:3000/").catch(console.log);
+  // }
+
+  const theLauncher = launcherDb[gameDb[obj.id].platform];
+
+  if (theLauncher.type === "retroarch") {
+    launch(buildRetroArchCommand(gameDb[obj.id], theLauncher));
+  }
+};
