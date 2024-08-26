4ab49341ffcace2029f8e598bdaea82e55e2665a
Simon W. Jackson <hello@simonwjackson.io>
2024-08-26 13:49:34 -0500

    chore: update justfiles

---
+++ justfile
@@ -0,0 +1,28 @@
+import? "packages/moonbeaming/justfile"
+import? "packages/moonbeam/justfile"
+
+# List all available tasks
+default:
+    @just --list
+
+# all: test
+#
+# # variadic arguments
+# test *names:
+#     ./run_tests.sh {{ names }}
+#
+# # Private -- omitted from just --list
+# _private-task:
+#     echo "Private task"
+#
+# doc:
+#     python .github/main.py generate
+#
+# lint:
+#     python .github/main.py lint
+#     stylua --check .
+#
+# # command argument as environment variable
+# greet $name:
+#     echo "Hello $name"
+# monorepo/justfile
--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,35 +1,37 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
   lib,
-  # You also have access to your flake's inputs.
   inputs,
-  # The namespace used for your flake, defaulting to "internal" if not set.
   namespace,
-  # All other arguments come from NixPkgs. You can use `pkgs` to pull shells or helpers
-  # programmatically or you may add the named attributes as arguments here.
   pkgs,
   mkShell,
   ...
-}:
-mkShell {
-  # Create your shell
-  packages = with pkgs; [
-    # Include BATS with support and assert libraries
-    (bats.withLibraries (p: [p.bats-support p.bats-assert]))
-    # Optional: Include these if you want them available in your shell
-    shellcheck # For shell script linting
-    jq
-    bash
-    bc
-    gum
-    entr
-  ];
+}: let
+  # Create a custom wrapped version of just
+  wrappedJust = pkgs.symlinkJoin {
+    name = "wrapped-just";
+    paths = [pkgs.just];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/just \
+        --add-flags "--working-directory \$(git rev-parse --show-toplevel)" \
+        --add-flags "--justfile \$(git rev-parse --show-toplevel)/justfile"
+    '';
+  };
+in
+  mkShell {
+    packages = [
+      (pkgs.bats.withLibraries (p: [p.bats-support p.bats-assert]))
+      pkgs.shellcheck
+      pkgs.bash
+      pkgs.entr
+      wrappedJust # This now correctly references the let-bound wrappedJust
+    ];
 
-  # Shell hook to set up the environment
-  shellHook = ''
-    echo "Welcome to the Bandwidth Calculator development environment!"
-    echo "BATS with support and assert libraries is available."
-    echo "Use 'bats' to run your tests."
-  '';
-}
+    shellHook = ''
+      echo "Welcome to the Bandwidth Calculator development environment!"
+      echo "BATS with support and assert libraries is available."
+      echo "Use 'bats' to run your tests."
+      echo "The 'just' command has been wrapped to always use the project root as the working directory"
+      echo "and the justfile in the project root."
+    '';
+  }
